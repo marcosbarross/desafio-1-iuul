2e4397e2b61c60240fc2af38d8a4eb45d145ba6a
Lucas <lucas.delima4@gmail.com>
2023-01-03 19:50:49 -0300

    Documentação das Classes Paciente e ListaPacientes

--- Odonto/Paciente/ListaPacientes.cs
+++ Odonto/Paciente/ListaPacientes.cs
@@ -1,40 +1,49 @@
 ﻿using Odonto.Extensions;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Odonto
 {
     public class ListaPacientes
     {
-        public Dictionary<string, Paciente> Pacientes { get; private set; }
-
+        /// <summary>
+        /// Define uma Lista de Pacientes da Clínica.
+        /// </summary>
         public ListaPacientes()
         {
             Pacientes = new Dictionary<string, Paciente>();
         }
+        public Dictionary<string, Paciente> Pacientes { get; private set; }
 
+        /// <summary>
+        /// Adiciona uma instância de Paciente a Lista de Pacientes.
+        /// </summary>
+        /// <param name="paciente">Representa o valor de uma instância de <see cref="Paciente"/> e não deve ser nula.</param>
         public void AdicionarNaLista(Paciente paciente)
         {
             Pacientes.Add(paciente.CPF, paciente);
         }
 
+        /// <summary>
+        /// Remove um Paciente da Lista de Pacientes.
+        /// </summary>
+        /// <param name="pacienteCPF">Representa o valor da chave corresponde ao Paciente que deverá ser removido.</param>
         public void RemoverDaLista(string pacienteCPF)
         {
             Pacientes.Remove(pacienteCPF);
         }
-
+        /// <summary>
+        /// Mostra todos os Pacientes ordenados, em ordem crescente, pelo valor do CPF
+        /// </summary>
         public void ListarPacientesPorCPF()
         {
             Pacientes.ImprimeDicionarioOrdenado(OrdenadoPor.CPF);
         }
+        /// <summary>
+        /// Mostra todos os Pacientes ordenados, em ordem crescente, pelo valor do Nome
+        /// </summary>
         public void ListarPacientesPorNome()
         {
             Pacientes.ImprimeDicionarioOrdenado(OrdenadoPor.Nome);
         }
-
     }
 }
