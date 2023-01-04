91a2d732225a700e1cf4ccd93b856dd4bfdf3dce
Lucas <lucas.delima4@gmail.com>
2023-01-03 21:17:23 -0300

    Correções no branch de develop após merge de duas melhorias.

--- Odonto/Agenda/Agenda.cs
+++ Odonto/Agenda/Agenda.cs
@@ -1,19 +1,20 @@
 ﻿using Odonto.Extensions;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Odonto
 {
+    /// <summary>
+    /// Define a Agenda da Clínica Odontológica.
+    /// </summary>
     public class Agenda
     {
-        public SortedList<DateTime, Agendamento> Agendamentos { get; private set; }
         public Agenda()
         {
             Agendamentos = new SortedList<DateTime, Agendamento>();
         }
+        public SortedList<DateTime, Agendamento> Agendamentos { get; private set; }
+
         public void AgendarConsulta(string CPF, DateTime diaConsulta, TimeSpan inicioConsulta, TimeSpan fimConsulta)
         {
 
